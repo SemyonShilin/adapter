578b81d65323ccfb2e12d862ddfca6846af7e4d6
SShilin <sshilin@fil-it.ru>
2018-05-24 10:48:42 +0300

    fix name

--- lib/adapter.ex
+++ lib/adapter.ex
@@ -1,9 +1,25 @@
 defmodule Adapter do
-  @moduledoc """
-  Adapter keeps the contexts that define your domain
-  and business logic.
+  use Application
 
-  Contexts are also responsible for managing your data, regardless
-  if it comes from the database, an external API or others.
-  """
+  def start(_type, _args) do
+    Envy.auto_load
+    import Supervisor.Spec
+    if System.get_env("MIX_ENV") == "dev", do: :observer.start
+
+    children = [
+      supervisor(Adapter.Repo, []),
+      {DynamicSupervisor, name: Adapter.MessengersSupervisor, strategy: :one_for_one},
+      {Adapter.Registry, name: Adapter.Registry},
+      supervisor(Adapter.Endpoint, [])
+    ]
+
+    opts = [strategy: :one_for_one, name: Adapter.Supervisor]
+
+    Supervisor.start_link(children, opts)
+  end
+
+  def config_change(changed, _new, removed) do
+    Adapter.Endpoint.config_change(changed, removed)
+    :ok
+  end
 end

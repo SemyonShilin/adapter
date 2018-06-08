439b071e76a52305bd32581200b2e1a4addea812
SShilin <sshilin@fil-it.ru>
2018-06-08 11:41:29 +0300

    receive message

--- web/controllers/webhook_controller.ex
+++ web/controllers/webhook_controller.ex
@@ -1,10 +1,17 @@
 defmodule Adapter.WebhookController do
   use Adapter.Web, :controller
 
+  alias Adapter.Bots
+  alias Adapter.Bots.Bot
+
   action_fallback Adapter.FallbackController
 
   def receive(conn, params) do
-    IO.inspect params
-    IO.inspect "from webhook"
+    bot = Bots.get_by_bot(uid: params["uid"]) || Bots.get_by_bot(token: params["uid"])
+    if bot do
+      conn |> put_status(200) |> send_resp(200, "")
+    else
+      conn |> put_status(404) |> send_resp(404, "")
+    end
   end
 end

d0a97909d01357bce0231c33a2a6507e480b1bc5
SShilin <sshilin@fil-it.ru>
2018-06-08 12:23:10 +0300

    try fix

--- web/controllers/webhook_controller.ex
+++ web/controllers/webhook_controller.ex
@@ -9,11 +9,17 @@ defmodule Adapter.WebhookController do
   def receive(conn, params) do
     bot = Bots.get_by_bot(uid: params["uid"]) || Bots.get_by_bot(token: params["uid"])
     if bot do
-      body = Adapter.InstanceGenServer.call_hub(params)
+      body = call_hub(params)
       Adapter.Registry.post_message(bot.uid, body)
       conn |> put_status(200) |> send_resp(200, "")
     else
       conn |> put_status(404) |> send_resp(404, "")
     end
+  end
+
+  defp call_hub(message) do
+    HTTPoison.start
+    {:ok, %HTTPoison.Response{body: body}} = HTTPoison.post System.get_env("DCH_POST"), Poison.encode!(message), [{"Content-Type", "application/json"}] |> IO.inspect
+    body
   end
 end

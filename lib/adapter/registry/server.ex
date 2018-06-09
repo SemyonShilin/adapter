5d284783ce847e302b2a393c6f9b8fb43cf31b0d
SShilin <sshilin@fil-it.ru>
2018-06-09 10:14:21 +0300

    fix

--- lib/adapter/registry/server.ex
+++ lib/adapter/registry/server.ex
@@ -206,12 +206,10 @@ defmodule Adapter.Registry.Server do
           bot = Adapter.Bots.get_by_bot(uid: bot)
           tuple = if System.get_env("BOT_ENV") == "development", do: {:post_message, message}, else: {:post_message_forward, bot.token, message}
 
-          Map.get(names, bot)
+          Map.get(names, bot.uid)
           |> Supervisor.which_children()
           |> Enum.find_value(find_pid)
-          |> IO.inspect
           |> GenServer.cast(tuple)
-          |> IO.inspect
           {:noreply, state}
         else
           {:noreply, state}

5b29cc3a734b96a739a769f8e2ed40ac11773c20
SShilin <sshilin@fil-it.ru>
2018-06-13 12:15:14 +0300

    try wobserver

--- lib/adapter.ex
+++ lib/adapter.ex
@@ -1,16 +1,28 @@
 defmodule Adapter do
   use Application
 
+  alias Plug.Adapters.Cowboy
+
   def start(_type, _args) do
     Envy.auto_load
     import Supervisor.Spec
     if System.get_env("MIX_ENV") == "dev", do: :observer.start
 
+    options = [
+      dispatch: [
+        {:_, [
+          {"/wobserver/ws", Wobserver.Web.Client, []},
+          {:_, Cowboy.Handler, {Adapter.Router, []}}
+        ]}
+      ],
+    ]
+
     children = [
       supervisor(Adapter.Repo, []),
       {DynamicSupervisor, name: Adapter.MessengersSupervisor, strategy: :one_for_one},
       {Adapter.Registry, name: Adapter.Registry},
-      supervisor(Adapter.Endpoint, [])
+      supervisor(Adapter.Endpoint, []),
+#      Cowboy.child_spec(:http, Adapter.Router, [], options),
     ]
 
     opts = [strategy: :one_for_one, name: Adapter.Supervisor]
